555f7d2fe46f6703506c4c5f1bcef01b191234de
Aditya Verma <adityaverma@Adityas-MacBook-Air.local>
2026-06-28 00:41:46 +0530

    auth middleware ,chat routes and controller

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
+
+/* Routes */
 const AuthRoutes=require('./routes/auth.routes');
+const ChatRoutes=require('./routes/chat.routes');
 
 const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-
+/* Using Routes */
 app.use('/api/auth',AuthRoutes);
+app.use('/api/chat',ChatRoutes);
 
 
 
