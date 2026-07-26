1ca2c2784c1c1763178199667843c3747c99e987
Aditya Verma <adityahosir@gmail.com>
2026-07-26 12:42:48 +0530

    Socket.io Middleware Connection Done

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,12 +1,32 @@
 const {Server} = require('socket.io');
+const cookie = require("cookie");
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model');
 
 function initSocketServer(httpserver){
 
-    const io = new Server(httpserver, {});
+    const io = new Server(httpserver, {})
+
+    io.use(async(socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+        if(!cookies.token){
+            return next(new Error('Authentication error:No Token provided'));
+        }
+
+        try {
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+            const user=await userModel.findById(decoded.id);
+
+            socket.user = user;
+            next();
+        }catch (err) {
+            return next(new Error('Authentication error'));
+        }
+    });
 
     io.on('connection', (socket) => {
-        console.log("New Socker Connection Established", socket.id);
-    
+        console.log("New Socket Connection Established", socket.id);
     });
 }
 
